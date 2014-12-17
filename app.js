adef208f47ae265a918b5185a49ea1a40853bd29
Jeremy Stephens <viking@pillageandplunder.net>
2014-12-17 16:43:35 +0000

    Refactor app and server
    - move app logic from server into app module
    - convert server into class object

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
-/**
- * Here we create an Express app instance that can
- * be required in various places.
- */
+var
+	environment = process.env.NODE_ENV || 'development',
+	config = require('./config');
 
 var
-    config = require('./config')[process.env.NODE_ENV],
-    express = require('express'),
-    cors = require('cors')
-    knex = require('knex')(config.database),
-    bookshelf = require('bookshelf')(knex);
-    app = module.exports = express();
+	express = require('express'),
+	cors = require('cors')
+	jade = require('jade'),
+	path = require('path'),
+	serveStatic = require('serve-static');
 
-app.use(cors());
-app.set('bookshelf', bookshelf);
+module.exports = function(bookshelf) {
+	var app = express();
+
+	app.use(cors());
+	app.set('bookshelf', bookshelf);
+	app.set('settings', config.global);
+	app.engine('jade', jade.__express);
+	app.use(serveStatic('./ui/public'));
+
+	app.get('/', function(req, res) {
+		delete require.cache[path.resolve('./versions/js.json')];
+		delete require.cache[path.resolve('./versions/css.json')];
+		
+		res.render('home.jade', {
+			title: 'Ping Pong',
+			metaDesc: 'Ping Pong',
+			clientUrl: config[environment].clientUrl,
+			clientPort: config[environment].clientPort,
+			wsPort: config[environment].wsPort,
+			winningViewDuration: config.global.winningViewDuration,
+			JSVersions: require('./versions/js'),
+			CSSVersions: require('./versions/css')
+		});
+	});
+
+	app.get('/leaderboard', function(req, res) {
+		bookshelf.Player.leaderboard().
+			then(function(players) {
+				res.json(players.toJSON());
+			});
+	});
+
+	return app;
+};
--- server.js
+++ server.js
@@ -1,100 +1,67 @@
 var
-    path = require('path'),
-    net = require('net'),
-    chalk = require('chalk'),
-    jade = require('jade'),
-    serveStatic = require('serve-static'),
-    environment = process.env.NODE_ENV = process.env.NODE_ENV || 'production',
-    app = require('./app.js'),
-    cardReader = require('./lib/cardReader'),
-    leaderboard = require('./lib/leaderboard');
+	environment = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+	config = require('./config');
 
-getConfig = require('./config');
-config = getConfig[environment];
-settings = getConfig.global;
+var
+	chalk = require('chalk'),
+	io = require('socket.io'),
+	GameController = require('./classes/GameController'),
+	ButtonController = require('./classes/ButtonController'),
+	CardReader = require('./lib/CardReader'),
+	Button = require('./lib/Button');
 
-app.set('settings', settings);
-app.engine('jade', jade.__express);
-app.use(serveStatic('./ui/public'));
-app.locals.config = config;
-app.locals.settings = settings;
+// Setup bookshelf
+var
+	knex = require('knex')(config[environment].database),
+	bookshelf = require('bookshelf')(knex);
 
-_ = require('underscore');
-io = require('socket.io');
-moment = require('moment');
-spark = require('sparknode');
-core = new spark.Core(settings.sparkCore);
+// Setup models
+require('./models/Player')(bookshelf);
+require('./models/Game')(bookshelf);
 
-gameController = require('./classes/gameController');
+function Server(config, environment, bookshelf) {
 
-game = {};
-player = {};
+	var _this = this;
 
-// Setup socketio
-io = io.listen(config.wsPort);
-console.log(chalk.green('Websocket Server: Listening on port ' + config.wsPort));
+	this.button1 = new Button(config.global.player1ButtonPin),
+	this.button2 = new Button(config.global.player2ButtonPin);
+	this.buttonController1 = new ButtonController(this.button1, 1),
+	this.buttonController2 = new ButtonController(this.button2, 2);
 
-io.configure(function() {
-    io.set('log level', 2);
-});
+	this.stats = require('./lib/stats')(bookshelf);
 
-app.get('/', function(req, res) {
-    
-    delete require.cache[path.resolve('./versions/js.json')];
-    delete require.cache[path.resolve('./versions/css.json')];
-    
-    res.render('home.jade', {
-        title: 'Ping Pong',
-        metaDesc: 'Ping Pong',
-        JSVersions: require('./versions/js'),
-        CSSVersions: require('./versions/css')
-    });
-    
-});
+	// Setup web server
+	this.app = require('./app')(bookshelf);
+	this.app.listen(config[environment].clientPort);
+	console.log(chalk.green('Web Server: Listening on port ' + config[environment].clientPort));
 
-app.get('/leaderboard', function(req, res) {
-    // This could use a streaming response instead
-    leaderboard.get(10)
-        .then(function(players) {
-            res.json(players.toJSON());
-        });
-});
+	// Setup socketio
+	this.io = io.listen(config[environment].wsPort);
+	console.log(chalk.green('Websocket Server: Listening on port ' + config[environment].wsPort));
 
-app.listen(config.clientPort);
-console.log(chalk.green('Web Server: Listening on port ' + config.clientPort));
+	this.io.configure(function() {
+	    _this.io.set('log level', 2);
+	});
 
-game = new gameController();
+	// Setup game controller
 
-game.feelersPingReceived();
+	this.gameController = new GameController(bookshelf, [this.buttonController1, this.buttonController2], this.stats, this.io.sockets);
 
-io.sockets.on('connection', function(client) {
-    game.reset();
-    game.clientJoined();
-    cardReader.connectionStatus();
-    client.on('fakeScored', game.feelerPressed); // Fake score event for easier testing
-});
+	this.io.sockets.on('connection', function(client) {
+	    _this.gameController.reset();
+	    _this.gameController.clientJoined();
+	    //client.on('fakeScored', game.feelerPressed); // Fake score event for easier testing
+	});
 
-core.on('scored', game.feelerPressed);
-core.on('ping', game.feelersPingReceived);    
-core.on('batteryLow', game.batteryLow);
+	// Setup hardware
+	this.cardReader = new CardReader();
 
-core.on('online', function() {
-    game.feelersOnline();
-    game.feelerStatus();
-    game.feelersPingReceived();
-});
+	this.cardReader.on('read', function(data) {
+	    console.log('New read', data);
+	    _this.gameController.addPlayerByRfid(data.rfid);
+	});
 
-cardReader.on('read', function(data) {
-    console.log('New read', data);
-    game.addPlayerByRfid(data.rfid);
-});
+};
 
-cardReader.on('err', game.cardReadError);
-
-cardReader.on('connect', function() {
-    io.sockets.emit('cardReader.connect');
-});
-
-cardReader.on('disconnect', function() {
-    io.sockets.emit('cardReader.disconnect');
-});
+var server = new Server(config, environment, bookshelf);
+module.exports = server;
