4786b974109110c56833e8c1832af19a334ff7ca
Jeremy Stephens <viking@pillageandplunder.net>
2014-12-17 16:46:28 +0000

    Alter config example for use with Raspberry Pi

--- config.example.js
+++ config.example.js
@@ -1,17 +1,12 @@
 module.exports = {
     development: {
-        clientUrl: undefined,
-        clientPort: undefined,
-        wsPort: undefined,
-        cardReaderPort: undefined,
+        clientUrl: 'http://localhost',
+        clientPort: 12345,
+        wsPort: 23456,
         database: {
-            client: 'mysql',
+            client: 'sqlite3',
             connection: {
-                host: undefined,
-                port: undefined,
-                user: undefined,
-                password: undefined,
-                database: undefined,
+		filename: __dirname + '/development.db'
             },
             migrations: {
                 directory: __dirname + '/migrations',
@@ -20,45 +15,30 @@ module.exports = {
         }
     },
     production: {
-        clientUrl: undefined,
-        clientPort: undefined,
-        wsPort: undefined,
+        clientUrl: 'http://localhost',
+        clientPort: 12345,
+        wsPort: 23456,
         database: {
-            client: 'mysql',
+            client: 'sqlite3',
             connection: {
-                host: undefined,
-                port: undefined,
-                user: undefined,
-                password: undefined,
-                database: undefined,
+		filename: __dirname + '/production.db'
             },
             migrations: {
                 directory: __dirname + '/migrations',
                 tableName: 'migrations'
             }
-        },
-        cardReaderPort: undefined
+        }
     },
     global: {
-        sparkCore: {
-            accessToken: undefined,
-            id: undefined
-        },
+	player1ButtonPin: 32,
+	player2ButtonPin: 36,
         serverSwitchLimit: 5, // How many points before service switches
         serverSwitchThreshold: 20, // When both players have reached this threshold, the server switches every time
         maxScore: 21,
         mustWinBy: 2,
         minPlayers: 2,
         maxPlayers: 2,
-        winningViewDuration: 12000, // The duration to show the winning view for before returning to the leaderboard
-        feelers: {
-            pingInterval: 5000,
-            pingThreshold: 250,
-            undoThreshold: 1500
-        },
-        cardReader: {
-            pingInterval: 30000,
-            pingThreshold: 250
-        }
+        buttonUndoThreshold: 1500,
+        winningViewDuration: 12000 // The duration to show the winning view for before returning to the leaderboard
     }
 };
